1559bf732cef310024a3defd6c15df0bee3fe7c5
RizqiRz <compoterindo@gmail.com>
2022-08-24 06:08:47 +0700

    Update and rename message/help.js to pesan/help.js

--- pesan/help.js
+++ pesan/help.js
@@ -17,7 +17,7 @@ function toCommas(x) {
 exports.allMenu = (ucapanWaktu, pushname, mundur, upload, download, ownerName, botName, jam, tanggal, runtime, isOwner, isPremium, sender, limitCount, limit, gcount, glimit, balance, prefix) => {
     return`${ucapanWaktu} ${pushname !== undefined ? pushname : 'Kak'}
 
-Hitung Mundur Idul Adha
+Hitung Mundur Tahun Baru
 ${mundur}
 
 *STATISTICS*
